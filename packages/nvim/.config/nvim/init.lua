c8134e54ccd723a4c381ce1661f9787bc1bea8db
Nikolay Bayborodin <nbbrdn@gmail.com>
2026-02-07 11:12:17 +1000

    Edin nvim config

---
+++ packages/nvim/.config/nvim/init.lua
@@ -0,0 +1,264 @@
+-- =========================
+-- Neovim config for Rust (Just Works edition)
+-- =========================
+
+-- Leader
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Basic options
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.termguicolors = true
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.updatetime = 200
+vim.opt.timeoutlen = 400
+
+-- Diagnostics UI
+vim.diagnostic.config({
+  virtual_text = { spacing = 2, prefix = "●" },
+  float = { border = "rounded" },
+  severity_sort = true,
+  underline = true,
+  update_in_insert = false,
+})
+
+-- =========================
+-- Bootstrap lazy.nvim
+-- =========================
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Convenience
+local map = vim.keymap.set
+
+-- =========================
+-- Global keymaps (defined immediately)
+-- These DO NOT depend on plugin load order.
+-- =========================
+
+-- Diagnostics
+map("n", "<leader>e", vim.diagnostic.open_float, { desc = "Diagnostics float" })
+map("n", "[d", vim.diagnostic.goto_prev, { desc = "Prev diagnostic" })
+map("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+
+-- Telescope (will auto-load when command runs)
+map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
+map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live grep" })
+map("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Buffers" })
+map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Help" })
+
+-- Trouble (will auto-load by command)
+map("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Trouble diagnostics" })
+map("n", "<leader>xq", "<cmd>Trouble qflist toggle<cr>", { desc = "Trouble quickfix" })
+
+-- Crates (works only in Cargo.toml, but key exists anyway)
+map("n", "<leader>ct", "<cmd>lua pcall(function() require('crates').toggle() end)<cr>", { desc = "Crates toggle" })
+map("n", "<leader>cr", "<cmd>lua pcall(function() require('crates').reload() end)<cr>", { desc = "Crates reload" })
+map("n", "<leader>cu", "<cmd>lua pcall(function() require('crates').update_all_crates() end)<cr>", { desc = "Crates update all" })
+
+-- DAP (keys exist; plugin loads when you first use it)
+map("n", "<F5>",  "<cmd>lua pcall(function() require('dap').continue() end)<cr>", { desc = "DAP continue" })
+map("n", "<F10>", "<cmd>lua pcall(function() require('dap').step_over() end)<cr>", { desc = "DAP step over" })
+map("n", "<F11>", "<cmd>lua pcall(function() require('dap').step_into() end)<cr>", { desc = "DAP step into" })
+map("n", "<F12>", "<cmd>lua pcall(function() require('dap').step_out() end)<cr>", { desc = "DAP step out" })
+map("n", "<leader>db", "<cmd>lua pcall(function() require('dap').toggle_breakpoint() end)<cr>", { desc = "DAP breakpoint" })
+map("n", "<leader>dB", "<cmd>lua pcall(function() require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) end)<cr>", { desc = "DAP conditional breakpoint" })
+map("n", "<leader>dr", "<cmd>lua pcall(function() require('dap').repl.open() end)<cr>", { desc = "DAP REPL" })
+
+-- =========================
+-- Plugins
+-- =========================
+require("lazy").setup({
+  -- Colorscheme
+  { "folke/tokyonight.nvim", lazy = false, priority = 1000, config = function() vim.cmd.colorscheme("tokyonight") end },
+
+  -- Statusline
+  { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }, event = "VeryLazy",
+    config = function() require("lualine").setup({ options = { theme = "auto" } }) end
+  },
+
+  -- Git signs
+  { "lewis6991/gitsigns.nvim", event = { "BufReadPre", "BufNewFile" }, config = function() require("gitsigns").setup() end },
+
+  -- Telescope
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" }, cmd = "Telescope", config = function() require("telescope").setup() end },
+
+  -- Trouble
+  { "folke/trouble.nvim", cmd = "Trouble", dependencies = { "nvim-tree/nvim-web-devicons" }, config = function() require("trouble").setup() end },
+
+  -- Treesitter (new API)
+  { "nvim-treesitter/nvim-treesitter", lazy = false, build = ":TSUpdate",
+    config = function()
+      local ts = require("nvim-treesitter")
+      ts.setup({})
+      -- enable treesitter highlighting automatically for common filetypes
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "rust", "toml", "lua", "vim", "vimdoc", "json", "yaml", "bash", "markdown" },
+        callback = function() pcall(function() vim.treesitter.start() end) end,
+      })
+    end
+  },
+
+  -- Mason
+  { "williamboman/mason.nvim", cmd = "Mason", config = function() require("mason").setup() end },
+  { "williamboman/mason-lspconfig.nvim", dependencies = { "williamboman/mason.nvim" }, event = { "BufReadPre", "BufNewFile" },
+    config = function() require("mason-lspconfig").setup({ ensure_installed = { "rust_analyzer" }, automatic_installation = true }) end
+  },
+
+  -- LSP
+  { "neovim/nvim-lspconfig", dependencies = { "williamboman/mason-lspconfig.nvim", "hrsh7th/cmp-nvim-lsp" }, event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local lspconfig = require("lspconfig")
+
+      local on_attach = function(_, bufnr)
+        local function bufmap(mode, lhs, rhs, desc)
+          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+        end
+        bufmap("n", "gd", vim.lsp.buf.definition, "Go to definition")
+        bufmap("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
+        bufmap("n", "gr", vim.lsp.buf.references, "References")
+        bufmap("n", "gi", vim.lsp.buf.implementation, "Implementation")
+        bufmap("n", "K", vim.lsp.buf.hover, "Hover")
+        bufmap("n", "<leader>rn", vim.lsp.buf.rename, "Rename")
+        bufmap("n", "<leader>ca", vim.lsp.buf.code_action, "Code action")
+        bufmap("n", "<leader>f", function() vim.lsp.buf.format({ async = true }) end, "Format")
+      end
+
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      lspconfig.rust_analyzer.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = { allFeatures = true },
+            checkOnSave = { command = "clippy", extraArgs = { "--", "-W", "clippy::all" } },
+            procMacro = { enable = true },
+          },
+        },
+      })
+
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+    end
+  },
+
+  -- Autocomplete
+  { "hrsh7th/nvim-cmp", event = "InsertEnter",
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "onsails/lspkind.nvim",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      local lspkind = require("lspkind")
+
+      cmp.setup({
+        snippet = { expand = function(args) luasnip.lsp_expand(args.body) end },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then luasnip.expand_or_jump()
+            else fallback() end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then luasnip.jump(-1)
+            else fallback() end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" }, { name = "luasnip" }, { name = "path" }, { name = "buffer" },
+        }),
+        formatting = { format = lspkind.cmp_format({ mode = "symbol_text", maxwidth = 50, ellipsis_char = "…" }) },
+        window = { completion = cmp.config.window.bordered(), documentation = cmp.config.window.bordered() },
+      })
+    end
+  },
+
+  -- crates.nvim
+  { "saecki/crates.nvim", event = { "BufRead Cargo.toml" }, dependencies = { "nvim-lua/plenary.nvim" },
+    config = function() require("crates").setup() end
+  },
+
+  -- DAP
+  { "mfussenegger/nvim-dap", lazy = true },
+  { "rcarriga/nvim-dap-ui", lazy = true, dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open() end
+      dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close() end
+      dap.listeners.before.event_exited["dapui_config"] = function() dapui.close() end
+    end
+  },
+  { "jay-babu/mason-nvim-dap.nvim", event = "VeryLazy", dependencies = { "williamboman/mason.nvim", "mfussenegger/nvim-dap" },
+    config = function() require("mason-nvim-dap").setup({ ensure_installed = { "codelldb" }, automatic_installation = true }) end
+  },
+
+  -- which-key (shows nice menu on Space)
+  {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  config = function()
+    local wk = require("which-key")
+    wk.setup()
+
+    -- Newer which-key API: add()
+    if wk.add then
+      wk.add({
+        { "<leader>f", group = "Find" },
+        { "<leader>x", group = "Trouble" },
+        { "<leader>c", group = "Crates" },
+        { "<leader>d", group = "Debug" },
+      })
+    end
+  end,
+},
+})
+
+-- =========================
+-- Rust-specific autocommands
+-- =========================
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "rust",
+  callback = function()
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = 0,
+      callback = function()
+        pcall(function() vim.lsp.buf.format({ async = false }) end)
+      end,
+    })
+  end,
+})
